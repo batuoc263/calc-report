28ab7ef8040e1ad00ce0a131d06dc5a70370be70
Dinh Chau Nguyen <nguyendinhchau705@gmail.com>
2021-05-05 11:22:23 +0700

    Init project and create index site

---
+++ views/site/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+    
+    <div class="body-content">
+        <p class="text-center lead">Trang web này là một nền tảng để thực hiện các tính toán thiết kế điển hình khác nhau về kỹ thuật xây dựng theo Tiêu chuẩn Việt Nam (TCVN). Kết quả tính toán là một báo cáo hoàn thiện được trình bày dưới dạng *.docx.</p>
+        <h1 style="font-size: 30px;" class="text-center">LỜI GIỚI THIỆU</h1>
+
+        <p class="lead">Hiện nay, trong bối cảnh công nghệ thông tin ngày càng phát triển thì nhu cầu cuộc sống cũng như việc ứng dụng công nghệ thông tin vào công việc của con người cũng được phát triển mạnh mẽ và ngày càng đa dạng hơn, đặc biệt trong lĩnh vực xây dựng yêu cầu các doanh nghiệp, cá nhân phải trang bị cho bản thân những phần mềm, công cụ tính toán. Nắm bắt được vấn đề này, tác giả đã lập nên website: titool.vn nhằm phục vụ cho các bạn sinh viên, kỹ sư …có thể dễ dàng thao tác và tính toán một cách hiệu quả, nhanh chóng. Một trong những mục tiêu chính của trang web này là trình bày hoàn chỉnh một báo cáo kỹ thuật về kết quả tính toán dựa trên số liệu đầu vào của người dùng. Các mẫu tính toán thực hiện theo các tình huống thiết kế được quy định cụ thể bởi văn bản quy phạm kỹ thuật.</p>
+
+        <p class="lead">Lưu ý rằng các mẫu tính toán trên trang web chỉ là một công cụ số học để thuận tiện cho việc tính toán thiết kế và trình bày kết quả. Tất cả các quy định về tính toán, kí hiệu, đơn vị, mô tả, giải thích và quy trình áp dụng chúng cũng sẽ được tìm thấy trong các TCVN liên quan. Bên cạnh đó lời nhắc ở trang nhập dữ liệu chỉ mang tính hướng dẫn và không được phép sử dụng các mẫu tính toán của trang web nếu bạn không phải là chuyên gia trong lĩnh vực xây dựng và chưa nghiên cứu các TCVN. Chúng tôi từ chối mọi trách nhiệm về các lỗi có thể xảy ra trong các mẫu tính toán. Mọi bảng tính hiển thị trên trang web đều được thực hiện “nguyên trạng” theo hướng dẫn tính toán của các TCVN. Người sử dụng đưa ra quyết định về khả năng áp dụng, trách nhiệm của mình đối với các tính toán.</p>
+        <p class="lead">Hiện tại, hầu hết các báo cáo được sắp xếp ở định dạng *.pdf và *.docx, sẽ đem lại sự tiện lợi cho người dùng để có thể tiếp cận với bố cục cũng như biểu thức toán học ở thuyết minh tính toán. Đồng thời, định dạng trình bày này sẽ cho phép nhiều chuyên gia đánh giá tính đúng đắn của các tính toán. Từ đó sẽ đảm bảo loại bỏ các sai sót có thể xảy ra, mạng lại các phép tính chính xác và đáng tin cậy. </p>
+        <p class="lead">Mặc dù trong quá trình thực hiện tác giả đã cố gắng trình bày đơn giản và tối ưu nhất để người dùng dễ dàng sử dụng nhưng không thể tránh khỏi những sai sót chủ quan. Rất mong sự góp ý của các chuyên gia, đồng nghiệp cho các tác giả thông qua e-mail: abc@gmail.com để website hoàn thiện hơn. Xin chân thành cảm ơn!
+        </p>
+        <p class="text-right lead"><strong>Tác giả</strong></p>
+    </div>
+</div>
