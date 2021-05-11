b9350b71f27cb28e5a3001289ad24f1795a3f93e
Dinh Chau Nguyen <nguyendinhchau705@gmail.com>
2021-05-11 14:47:16 +0700

    Fix height frame

--- views/tinh-toan/result.php
+++ views/tinh-toan/result.php
@@ -11,6 +11,6 @@ $this->params['breadcrumbs'][] = ['label' => 'Tính toán', 'url' => ['index']];
 ?>
 <div class="tinhtoan-content">
     
-    <embed width="100%" height="100%" src="https://view.officeapps.live.com/op/view.aspx?src=<?= $_SERVER['HTTP_HOST'] . $filePath ?>" frameborder="0" />
+    <embed width="100%" height="700px" src="https://view.officeapps.live.com/op/view.aspx?src=<?= $_SERVER['HTTP_HOST'] . $filePath ?>" frameborder="0" />
             
 </div>
