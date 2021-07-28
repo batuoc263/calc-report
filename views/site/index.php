58e8cb0875de78310ae30319d572182467bfdc95
Dinh Chau Nguyen <nguyendinhchau705@gmail.com>
2021-07-28 10:02:06 +0700

    Update UI

--- views/site/index.php
+++ views/site/index.php
@@ -16,23 +16,21 @@ $this->title = $model->title;
             </div>
         </div>
         <div class="row">
-            <div class="col-md-8 gioi-thieu">
+            <div class="col-md-12 gioi-thieu">
                 <h4 class="text-center"><strong>GIỚI THIỆU</strong></h4>
                 <p class="p_gioithieu">Hiện nay, trong bối cảnh công nghệ thông tin ngày càng phát triển thì nhu cầu ứng dụng công nghệ thông tin vào công việc của con người cũng được phát triển mạnh mẽ và đa dạng hơn, đặc biệt trong các lĩnh vực kỹ thuật nói chung và xây dựng nói riêng. Nắm bắt được vấn đề này, tác giả đã lập nên website: <a href="www.oxyz.vn"><strong>www.oxyz.vn</strong></a> nhằm phục vụ cho việc học tập của các bạn sinh viên, kỹ sư xây dựng…có thể dễ dàng thao tác và tính toán một cách hiệu quả, nhanh chóng. Một trong những mục tiêu chính của website là trình bày hoàn chỉnh một báo cáo kỹ thuật về kết quả tính toán dựa trên thông số đầu vào của người dùng. Các mẫu tính toán thực hiện theo các tình huống thiết kế được quy định cụ thể bởi văn bản quy phạm kỹ thuật. </p>
                 <p class="p_gioithieu">Các mẫu tính toán này chỉ là một công cụ số học để thuận tiện cho việc tính toán thiết kế và trình bày kết quả. Tất cả các quy định về tính toán, kí hiệu, đơn vị, mô tả, giải thích và quy trình áp dụng chúng cũng sẽ được tìm thấy trong các TCVN liên quan. Bên cạnh đó, lời nhắc ở trang nhập dữ liệu chỉ mang tính hướng dẫn, tham khảo.</p>
+            </div>
+        </div>
+        <div class="row" style="position: relative;">
+            <div class="col-md-12 img-home-mt">
+                <img src="/images/home/home_qr.jpg" class="img-responsive" alt="Trang chủ">
+            </div>
+            <div class="col-md-7 text-top-left">
                 <p class="p_gioithieu">Lưu ý không được phép sử dụng các mẫu tính toán này nếu bạn không theo học chuyên ngành xây dựng và chưa nghiên cứu các TCVN. Tác giả từ chối mọi trách nhiệm về các lỗi có thể xảy ra trong các mẫu tính toán. Mọi bảng tính hiển thị trên website đều được thực hiện “nguyên trạng” theo hướng dẫn tính toán của các TCVN. Người sử dụng đưa ra quyết định về khả năng áp dụng, trách nhiệm của mình đối với các tính toán.</p>
                 <p class="p_gioithieu">Hiện tại, các báo cáo được sắp xếp ở định dạng *.docx, sẽ đem lại sự tiện lợi cho người dùng để có thể tiếp cận với bố cục cũng như biểu thức toán học ở thuyết minh tính toán. Đồng thời, định dạng trình bày này sẽ cho phép nhiều chuyên gia đánh giá tính đúng đắn của các bảng tính. Từ đó sẽ đảm bảo loại bỏ các sai sót có thể xảy ra, mang lại các phép tính chính xác và đáng tin cậy.</p>
                 <p class="p_gioithieu">Mặc dù trong quá trình thực hiện tác giả đã hết sức cố gắng nhưng chắc rằng vẫn không thể tránh khỏi những sai sót chủ quan. Rất mong sự góp ý của các chuyên gia, đồng nghiệp, các bạn sinh viên cho tác giả thông qua e-mail: <strong> <a href = "mailto:oxyz@gmail.com">oxyz@gmail.com</a> </strong> để website tiếp tục hoàn thiện hơn. Chân thành cảm ơn!</p>
-                <p class="text-right">Tác giả</p>
-                <p class="text-right">LÊ VĂN TRỌNG</p>
-            </div>
-            <div class="col-md-4 text-center">
-                <img src="/images/home/h1.jpg" class="sidebar-img" alt="Hình 1">
-                <img src="/images/home/h2.jpg" class="sidebar-img" alt="Hình 2">
-                <img src="/images/home/h3.jpg" class="sidebar-img" alt="Hình 3">
-                <img src="/images/home/h4.jpg" class="sidebar-img" alt="Hình 4">
-                <img src="/images/home/h5.jpg" class="sidebar-img" alt="Hình 5">
-                <!-- <img src="/images/home/h6.jpg" class="sidebar-img" alt="Hình 6"> -->
+                <p class="text-right">Tác giả: LÊ VĂN TRỌNG</p>
             </div>
         </div>
     </div>
